a2b57982662a89b8d51ca912501511f3fbf984f7
theeps <33244682+theeps@users.noreply.github.com>
2021-10-08 22:14:02 +0530

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,8 @@ from student import Student
 
 student1 = Student('Amal')
 student2 = Student('Kamal')
+student3 = Student('Chapie')
 print('Hello world')
 print(student1.marks)
 print(student2.marks)
+print(student3.marks)
